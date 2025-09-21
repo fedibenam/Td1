17f2b9b0f0270c2b56bff552b98156bd883bc69c
Fedi Ben Hamouda <fedibenhamchesd@gmail.com>
2025-09-21 22:13:57 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.zoo.ZooManagement;
+
+public class Main {
+    public static void main(String[] args) {
+        ZooManagement zoo = new ZooManagement();
+    }
+}
---
+++ src/main/java/org/example/zoo/ZooManagement.java
@@ -0,0 +1,25 @@
+package org.example.zoo;
+
+import java.util.Scanner;
+
+public class ZooManagement {
+    int nbrCages;
+    String zooName;
+
+    public ZooManagement() {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println(" name of the zoo:");
+            zooName = scanner.nextLine();
+        } while (zooName.isEmpty());
+        do {
+            System.out.println(" number of cages:");
+            while (!scanner.hasNextInt()) {
+                System.out.println("write number ");
+                scanner.next();
+            }
+            nbrCages = scanner.nextInt();
+        } while (nbrCages <= 0);
+        System.out.println("This is " + zooName + " with " + nbrCages + " cages.");
+    }
+}
